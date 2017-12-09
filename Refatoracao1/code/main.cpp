bb080b8773c0d44f4c4ca54bad5c3bbb98900131
joaopaulocastilho <joao.pkc@gmail.com>
2017-12-09 13:35:37 -0200

    Parece que está determinizando legal

--- Refatoracao1/code/main.cpp
+++ Refatoracao1/code/main.cpp
@@ -3,14 +3,22 @@
 
 int main(void) {
   FILE *entrada;
-  if (!(entrada = fopen("input/input7.txt", "r"))) {
+  if (!(entrada = fopen("input/input8.txt", "r"))) {
     printf("Erro ao abrir o arquivo!");
     return 0;
   }
   mapearGramatica(entrada);
   construirAfndGramatica(entrada);
+  puts("AUTOMATO CONSTRUIDO");
   imprimeAutomato(automato);
   minimiza();
+  puts("AUTOMATO MINIMIZADO");
+  imprimeAutomato(automato);
+  determiniza();
+  puts("AUTOMATO DETERMINIZADO");
+  imprimeAutomato(automato);
+  minimiza();
+  puts("AUTOMATO MINIMIZADO E DETERMINIZADO");
   imprimeAutomato(automato);
   return 0;
 }
