c4ad8131b8426a4d13858363313c03741d101d21
chabat <felipechabat@gmail.com>
2017-12-19 21:40:20 -0200

    Construcao do analisador sintatico iniciada

--- Compiladores/Projeto-1/run.sh
+++ Compiladores/Projeto-1/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ code/main.cpp code/automato.cpp code/determinimiza.cpp code/lexica.cpp code/utilidades.cpp -Wall -O2
+g++ -std=c++11 code/main.cpp code/automato.cpp code/determinimiza.cpp code/lexica.cpp code/utilidades.cpp -Wall -O2
 time ./a.out
